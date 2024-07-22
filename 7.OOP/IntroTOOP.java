c04731588a1b00a342e966cd161be5b4dadd71c3
Leonard Jombo <158037209+lenardjombo@users.noreply.github.com>
2024-07-22 20:39:51 +0300

    Add files via upload

---
+++ 7.OOP/IntroTOOP.java
@@ -0,0 +1,18 @@
+
+/*
+ **************Classes and objects***********************************************************
+ A class defines the data fields and actions of an object
+ Class also defines structure of objects
+ Classes act as blueprint of objects
+ An object is an instance of an class
+ Objects of the same type are created/instantiated from the same class
+ A java class uses variables to define the data fields and methods to define actions. . 
+There are special methods called constructors
+A constructor can do anything ,it is simply a method,but it is designed to do initializing actions,
+such as initializing the data fields of an object.
+A constructor doesn't have a return type
+The parameters of a constructor should have the same name as data fields of the class
+Default constructor takes no parameters
+*********************************************************************************************
+
+ */
