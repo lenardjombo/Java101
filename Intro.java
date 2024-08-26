6d7b5e24dfb03d5e5a100e2487f3dfaa979c9c0c
Leonard Jombo <158037209+lenardjombo@users.noreply.github.com>
2024-08-26 09:19:31 -0400

    Create Intro.java

---
+++ Intro.java
@@ -0,0 +1,76 @@
+public class Intro {
+    public static void main(String[] args) {
+        /*
+         * </>Features of Java ☕ </>
+         * 1.Simple
+         * 2.Object-Oriented
+         * 3.Portable
+         * 4.Platform independent
+         * 5.Secured
+         * 6.Robust
+         * 7.Architecture neutral
+         * 8.Interpreted
+         * 9.High Performance
+         * 10.Multithreaded
+         * 11.Distributed
+         * 12.Dynamic
+         * 
+         * *****Java is an object-oriented programming language. Everything in Java is
+         * an
+         * object. Object-oriented means we organize our software as a combination of
+         * different types of objects that incorporate both data and behavior.*****
+         * 
+         * Basic concepts of OOPs are:
+         * Object
+         * Class
+         * Inheritance
+         * Polymorphism
+         * Abstraction
+         * Encapsulation
+         * 
+         * ***Java code can be executed on multiple platforms, for example, Windows,
+         * Linux, Sun Solaris, Mac/OS, etc. Java code is compiled by the compiler and
+         * converted into bytecode. This bytecode is a platform-independent code because
+         * it can be run on multiple platforms, i.e., Write Once and Run Anywhere
+         * (WORA).
+         * 
+         * VM (Java Virtual Machine) is an abstract machine. It is called a virtual
+         * machine because it doesn't physically exist. It is a specification that
+         * provides a runtime environment in which Java bytecode can be executed. It can
+         * also run those programs which are written in other languages and compiled to
+         * Java bytecode.
+         * 
+         * The JVM performs the following main tasks:
+         * 1.Loads code
+         * 2.Verifies code
+         * 3.Executes code
+         * 4.Provides runtime environment
+         * 
+         * JRE::
+         * JRE is an acronym for Java Runtime Environment. It is also written as Java
+         * RTE. The Java Runtime Environment is a set of software tools which are used
+         * for developing Java applications. It is used to provide the runtime
+         * environment. It is the implementation of JVM. It physically exists. It
+         * contains a set of libraries + other files that JVM uses at runtime.
+         * 
+         * JDK::
+         * JDK is an acronym for Java Development Kit. The Java Development Kit (JDK) is
+         * a software development environment which is used to develop Java applications
+         * and applets. It physically exists. It contains JRE + development tools.
+         * he JDK contains a private Java Virtual Machine (JVM) and a few other
+         * resources such as an interpreter/loader (java), a compiler (javac), an
+         * archiver (jar), a documentation generator (Javadoc), etc. to complete the
+         * development of a Java Application.
+         * 
+         * => Variable
+         * A variable is the name of a reserved area allocated in memory.
+         * 
+         * Types of Variables
+         * There are three types of variables in Java:
+         * 1.local variable
+         * 2.instance variable
+         * 3.static variable
+         */
+        System.out.println("Hi");
+    }
+}
